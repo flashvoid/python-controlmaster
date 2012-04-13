11316e759122017b149e6a37aa3b473876d3d177
Stas A. Kraev <mail@kraev.me>
2012-04-13 13:39:49 +0700

    first commit

---
+++ controlmaster.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+from cStringIO import StringIO
+import sys
+import os
+import commands
+import string
+import random
+import subprocess
+
+class controlmaster():
+  host = ''
+  status = ''
+  master_socket = ''
+  controldir = ''
+  debug = False
+  def __init__(self,hostname,master_socket='',debug=False):
+    self.ifdebug("in init")
+    self.host = hostname
+    self.check_control_dir()
+    self.stdout = StringIO()
+    self.stderr = StringIO()
+    if master_socket=='':
+      rnd=''.join(random.choice(string.letters) for i in xrange(10))
+      self.master_socket = self.controldir + '/' + rnd
+    else:
+      self.master_socket = self.controldir + '/' + master_socket
+    self.debug=debug
+
+  def ifdebug(self,args):
+    if self.debug:
+      print args
+
+  def cmd(self,args):
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    sys.stdout = self.stdout
+    sys.stderr = self.stderr
+    self.ifdebug("in cmd: executing %s" % args)
+    try:
+      retcode = subprocess.call(args)
+      if retcode < 0:
+        print >>sys.stderr,"in cmd: Child was terminated", -retcode
+    except OSError, e:
+      print >>sys.stderr,"in cmd: Execution failed:", e
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    return retcode
+
+  def checkauth(self):
+    self.ifdebug("in checkauth")
+    if not os.path.exists(self.master_socket):
+      self.ifdebug("in checkauth: FAIL: NOFILE")
+      return False
+    status = self.cmd(['ssh','-S',self.master_socket,'-O','check','go'])
+    if status != 0:
+      self.ifdebug("in checkauth: FAIL: NOCONN")
+      return False
+    return True
+
+  def connect(self):
+    self.ifdebug("in connect")
+    if self.checkauth(): return True
+    status = self.cmd(['ssh', '-fNMS', self.master_socket, self.host])
+    if status == 0: 
+      return True
+    else:
+      print "in connect: Fail : %s" % status
+      self.disconnect()
+      return False
+      
+  def disconnect(self):
+    self.ifdebug("in disconnect")
+    if not self.checkauth(): return True
+    status = self.cmd(['ssh','-S',self.master_socket,'-O', 'exit', 'go'])
+    if status == 0: 
+      return True
+    else:
+      print "in disconnect: Fail : %s" % status
+      return False
+
+  def put(self,src,dst):
+    if not self.checkauth(): return False
+    self.ifdebug("in put")
+    status = self.cmd(['scp','-o controlpath='+self.master_socket,src,'go:'+dst])
+    if status == 0: 
+      return True
+    else:
+      print "in put: Fail : %s" % status
+      return False
+
+  def get(self,src,dst):
+    if not self.checkauth(): return False
+    self.ifdebug("in get")
+    status = self.cmd(['scp','-o controlpath='+self.master_socket,'go:'+src,dst])
+    if status == 0: 
+      return True
+    else:
+      print "in get: Fail : %s" % status
+      return False
+
+  def rput(self):
+    if not self.checkauth(): return False
+    self.ifdebug("in rput")
+    status = self.cmd(['scp','-r','-o controlpath='+self.master_socket,src,'go:'+dst])
+    if status == 0: 
+      return True
+    else:
+      print "in rput: Fail : %s" % status
+      return False
+
+  def rget(self):
+    if not self.checkauth(): return False
+    self.ifdebug("in rget"  )
+    status = self.cmd(['scp','-r','-o controlpath='+self.master_socket,'go:'+src,dst])
+    if status == 0: 
+      return True
+    else:
+      print "in rget: Fail : %s" % status
+      return False
+ 
+  def exe(self,command):
+    self.ifdebug("in exe")
+    if not self.checkauth(): return False
+    status = self.cmd(['ssh','-S',self.master_socket,'go',command])
+    if status == 0: 
+      print self.stdout.getvalue()
+      return status
+    else:
+      print "in exe: Fail : %s" % status
+      return False
+
+  def check_control_dir(self,
+                        controldir=os.environ['HOME']+"/.controlmaster"):
+    self.ifdebug("in check_control_dir : controldir = %s" % controldir)
+    if not os.path.isdir(controldir):
+      os.makedirs(controldir)
+    self.controldir = controldir
+
+  def create_master_socket(self):
+    self.ifdebug("in create_master_socket")
+
+
+if __name__ == '__main__':
+  if len(sys.argv)>1:
+    host=sys.argv[1] 
+  if len(sys.argv)>2:
+    debug=sys.argv[2] 
+  else:
+    debug=False
+  ssh = controlmaster(host,debug=debug)
+  ssh.connect()
+  ssh.exe('uptime')
+  ssh.disconnect()
